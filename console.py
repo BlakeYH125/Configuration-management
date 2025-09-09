3fd4a4649ac347c293381da94265aadc09e868a9
Nikolay <blakeyh125@yandex.ru>
2025-09-09 21:51:34 +0300

    Добавлен скролл по строкам, учтен ввод пустой строки

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 import shlex
 from tkinter import *
 import tkinter.ttk as ttk
+from tkinter.scrolledtext import ScrolledText
 
 window = Tk()
 window.title("VFS")
@@ -13,16 +14,19 @@ frame.pack(side=BOTTOM, fill=X, padx=5, pady=5)
 entry = Entry(frame, width=80)
 entry.pack(side=LEFT, fill=X, expand=True, padx=(0, 5))
 
-output_label = Label(window, text="", anchor="w", justify=LEFT)
-output_label.pack(side=TOP, fill=X, padx=5, pady=5)
+text = ScrolledText(window, wrap=WORD, height=20, state=DISABLED)
+text.pack(side=TOP, fill=X, padx=5, pady=5)
 
 allText = []
 
 def PressEnter(event):
-    inputLine = shlex.split(entry.get())
-    command = inputLine[0]
-    output = ""
-
+    inputLine = entry.get()
+    if len(inputLine) != 0:
+        inputLine = shlex.split(inputLine)
+        command = inputLine[0]
+        output = ""
+    else:
+        command = ""
     match command:
         case "cd":
             if len(inputLine) > 1:
@@ -32,9 +36,12 @@ def PressEnter(event):
 
         case "ls":
             if len(inputLine) > 1:
-                output = "C:\\ls Parameters: " + " ".join(inputLine[1:])
+                output = "C:\\ls Parameters: " + ", ".join(inputLine[1:])
             else:
                 output = "C:\\ls"
+
+        case "":
+            output = "C:\\"
 
         case "exit":
             exit(0)
@@ -43,9 +50,11 @@ def PressEnter(event):
             output = "C:\\Неверная команда"
 
     allText.append(output + "\n")
-    output_label.config(text="".join(allText))
+    text.config(state=NORMAL)
+    text.insert(END, output + "\n")
+    text.see(END)
+    text.config(state=DISABLED)
     entry.delete(0, END)
 entry.bind("<Return>", PressEnter)
 
 window.mainloop()
-
