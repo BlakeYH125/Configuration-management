dffbe6bcafd2e5c20cddaaa6b22e2b330d71e7a6
Nikolay <blakeyh125@yandex.ru>
2025-09-09 22:03:59 +0300

    Улучшение внешнего вида и читаемости кода

--- console.py
+++ console.py
@@ -3,21 +3,6 @@ from tkinter import *
 import tkinter.ttk as ttk
 from tkinter.scrolledtext import ScrolledText
 
-window = Tk()
-window.title("VFS")
-window.geometry("800x400")
-tab_control = ttk.Notebook(window)
-
-frame = Frame(window)
-frame.pack(side=BOTTOM, fill=X, padx=5, pady=5)
-
-entry = Entry(frame, width=80)
-entry.pack(side=LEFT, fill=X, expand=True, padx=(0, 5))
-
-text = ScrolledText(window, wrap=WORD, height=20, state=DISABLED)
-text.pack(side=TOP, fill=X, padx=5, pady=5)
-
-allText = []
 
 def PressEnter(event):
     inputLine = entry.get()
@@ -49,12 +34,25 @@ def PressEnter(event):
         case _:
             output = "C:\\Неверная команда"
 
-    allText.append(output + "\n")
     text.config(state=NORMAL)
     text.insert(END, output + "\n")
     text.see(END)
     text.config(state=DISABLED)
     entry.delete(0, END)
-entry.bind("<Return>", PressEnter)
 
+window = Tk()
+window.title("VFS")
+window.geometry("800x400")
+tab_control = ttk.Notebook(window)
+
+frame = Frame(window)
+frame.pack(side=BOTTOM, fill=X, padx=5, pady=5)
+
+entry = Entry(frame, width=80)
+entry.pack(side=LEFT, fill=X, expand=True, padx=(0, 5))
+
+text = ScrolledText(window, wrap=WORD, height=20, state=DISABLED)
+text.pack(side=TOP, fill=X, padx=5, pady=5)
+
+entry.bind("<Return>", PressEnter)
 window.mainloop()
